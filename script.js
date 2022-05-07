31f9bc6413a46b851c380911cbceba40589baa2a
Goundar <dalberto.pj@gmail.com>
2022-05-07 22:45:12 +0200

    Better indentation

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-
-  
-  var typed = new Typed(".typed-strings", {
-      strings: ["For those I have lost...","For those I can yet save..."],
-      typeSpeed: 50,
-      backSpeed: 50,
-      loop: false
-  })
-
-
+var typed = new Typed(".typed-strings", {
+  strings: ["For those I have lost...", "For those I can yet save..."],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: false,
+});
