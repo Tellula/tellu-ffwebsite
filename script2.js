03ea0c1902d6b43d6d7b6b7f9c83e9f88ef5149c
Tellula <52374386+Tellula@users.noreply.github.com>
2022-05-21 16:54:13 +0200

    Video player js, css enhancements

--- script2.js
+++ script2.js
@@ -1,27 +1,20 @@
-// Get element
-// let youtubeEmbedElement = document.getElementById("youtubeEmbed");
-
-// Add YouTube API script
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
-// var videoId = youtubeEmbedElement.dataset.videoId;
-var startSeconds = 4;
+var startSeconds = 5;
 var endSeconds = 340;
 var nextButton = document.getElementById("nextButton");
 
-onYouTubeIframeAPIReady = () => {
-    
+function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
-    videoId: "vWgpJ0k820w", // YouTube Video ID
     playerVars: {
       autoplay: 1, // Auto-play the video on load
       autohide: 1, // Hide video controls when playing
-      disablekb: 1,
-      controls: 1, // Hide pause/play buttons in player
+      disablekb: 0 ,
+      controls: 0, // Hide pause/play buttons in player
       showinfo: 0, // Hide the video title
       modestbranding: 0, // Hide the Youtube Logo
       loop: 0, // Run the video in a loop
@@ -29,13 +22,18 @@ onYouTubeIframeAPIReady = () => {
       rel: 0,
       enablejsapi: 1,
       start: startSeconds,
-      end: endSeconds,
-    },
+      widget_referrer: "https://tellula.github.io/tellu-ffwebsite/MusicPlayer.html"
+      },
 
-  
     events: {
       onReady: function (e) {
         // e.target.mute();
+        e.target.loadPlaylist({
+          list: "PLm6gHyIWRgty-L9Qz8l3_ZM_QC92wKdZn",
+          startSeconds: 5,
+          autoplay: 1,
+        });
+        e.target.setVolume(50);
         e.target.playVideo();
       },
       onStateChange: function (e) {
@@ -50,7 +48,5 @@ onYouTubeIframeAPIReady = () => {
       },
     },
   });
-    
-
-};
-
+}
+// onYouTubeIframeAPIReady();
